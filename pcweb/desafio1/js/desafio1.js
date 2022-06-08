b717c9339fb3cece3e9b92789cf6e45be3ee5cd9
Paulo Affonso <plpaulo25affonso@outlook.com.br>
2022-06-08 13:03:17 -0300

    ADD: Desafio 1 e 2

---
+++ pcweb/desafio1/js/desafio1.js
@@ -0,0 +1,12 @@
+const newDate = new Date();
+console.log(date)
+const date = document.querySelector(".input1");
+console.log(dateNas);
+const confirm = document.querySelector(".confirm1");
+
+const dateNas = () => {
+    const dateInput = dateNas.value;
+    console.log(dateInput)
+}
+
+confirm.addEventListener("click", dateNas)
