e1d0d39fcb208fbc0cedbe105d2cdcc6b9df4f02
Paulo Affonso <plpaulo25affonso@outlook.com.br>
2022-09-27 22:56:55 -0300

    e sobre isso e não tá nada bem

--- pcweb/catalogo_filmes/js/app.js
+++ pcweb/catalogo_filmes/js/app.js
@@ -12,9 +12,9 @@ let xhttp = new XMLHttpRequest();
 
 let url = "https://rafaelescalfoni.github.io/desenv_web/filmes.json";
 
-let criaGenero = filme => filme.generos.reduce((elementos, item) => elementos + `<li>${item}</li>`, "")
+let criaGenero = filme => filme.generos.reduce((elementos, item) => elementos + `<li class="lista">${item}</li>`, "")
 
-let criaElenco = filme => filme.elenco.reduce((elementos, item) => elementos + `<li>${item}</li>`, "")
+let criaElenco = filme => filme.elenco.reduce((elementos, item) => elementos + `<li class="lista">${item}</li>`, "")
 
 let rating = filme => {
     switch (filme) {
@@ -37,29 +37,35 @@ let rating = filme => {
 
 let criaOpiniao = filme => {
     return filme.opinioes.reduce((elementos, item) => {
-        return elementos + `<li>${rating(item.rating)}</li><li>${item.descricao}</li>`}, "")
+        return elementos + `<div class="ratingBox"><li class="listaRating">${item.descricao}</li><li class="listaRating">${rating(item.rating)}</li> </div>`}, "")
 }
 
 let criaApresentacao = (filme) => {
     return `
     <div class="filme">
-        <img src="${filme.figura}" alt="${filme.titulo}">
-        <h3>${filme.titulo}</h3>
-        <p class="descricao">${filme.resumo}</p>
-        <div class="genero">
-            <ul>
-                ${criaGenero(filme)}
-            </ul>
+        <img class="img" src="${filme.figura}" alt="${filme.titulo}">
+        <div>
+            <h2>${filme.titulo}</h2>
+            <p class="descricao">${filme.resumo}</p>
+            <div class="genero">
+                <p class="name">Gêneros: </p>
+                <ul>
+                    ${criaGenero(filme)}
+                </ul>
+            </div>
+            <div class="elenco">
+                <p class="name">Elenco: </p>
+                <ul>
+                    ${criaElenco(filme)} 
+                </ul>
+            </div>
+            <div class="rating">
+                <p class="name">Comentários: </p>
+                <ul>
+                    ${criaOpiniao(filme)}
+                </ul>
+            </div>
         </div>
-        <div class="elenco">
-            <ul>
-                ${criaElenco(filme)} 
-            </ul>
-        </div>
-        <div class="titulos semelhantes">
-                ${criaOpiniao(filme)}
-        </div>
-        
     </div>`
 }
 
